aace711ad7a9ccfde2e0d9fa78012389e1d13d71
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-07-03 13:35:28 +0530

    add: login and forgot pass

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { Routes, Route } from'react-router-dom'
 
 import Login from './pages/Login';
 import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
 
 const App = () => {
   return (
     <>
-    <div className='bg-red-600 text-[2rem]'>App</div>
+    {/* <div className='bg-red-600 text-[2rem]'>App</div> */}
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/forgot' element={<ForgotPassword />} />
     </Routes>
     </>
   )
