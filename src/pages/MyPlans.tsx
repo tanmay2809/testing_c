06e712fd8abfcfc5af9b6036da1f4a7defecbc74
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-07-16 18:26:04 +0530

    updates: settings sizes updated to rem

--- src/pages/MyPlans.tsx
+++ src/pages/MyPlans.tsx
@@ -9,19 +9,19 @@ const MyPlans = () => {
   return (
     <>
       <div className="w-full h-fit relative font-inter">
-        <div className=" w-[93%] h-fit px-[2rem] py-[1rem] flex flex-col items-center justify-center gap-10 ml-[7%]  ">
+        <div className="w-[93%] h-fit px-[2rem] py-[1rem] flex flex-col items-center justify-center gap-10 ml-[7%]">
           <div className="w-full px-6 py-4 bg-[#F1F7FF]">
             <div className="w-full flex flex-row justify-between">
               <div className="flex flex-col">
-                <h1 className="text-[24px] font-[500]">Free Trial</h1>
-                <p className="text-[16x]">Current Plan</p>
+                <h1 className="text-[1.3rem] font-[600]">Free Trial</h1>
+                <p className="text-[1rem]">Current Plan</p>
               </div>
               <div className="w-fit h-fit">
-                <button className="w-[150px] flex flex-row justify-center items-center gap-2 bg-[#004AAD] text-white px-1 py-2 text-lg font-[500] rounded-[8px]">
+                <button className="w-[8rem] flex flex-row justify-center items-center gap-2 bg-[#004AAD] text-white  py-1 text-lg font-[500] rounded-[0.5rem]">
                   Upgrade
                 </button>
                 <svg
-                  className="relative -top-14 -right-[8.4rem] w-7 h-6"
+                  className="relative -top-[3rem] -right-[7.1rem] w-7 h-6"
                   width="32"
                   height="34"
                   viewBox="0 0 32 34"
@@ -43,69 +43,71 @@ const MyPlans = () => {
                     height="24.54"
                     rx="11.19"
                     stroke="white"
-                    stroke-width="5"
+                    strokeWidth="5"
                   />
                   <path
                     d="M18.6236 14.7973L24.3745 13.2897L21.785 22.8177L21.7842 22.8206L21.7671 22.82H21.75H9.16H9.15184L9.14369 22.8201L7.41392 13.2914L7.41399 13.2914L7.41201 13.2811L7.42563 13.2861L7.44075 13.291L12.7608 15.021L13.6558 15.312L14.0005 14.4362L15.7095 10.0944L17.4486 14.2186L17.7853 15.0171L18.6236 14.7973ZM20.38 1H11C8.34783 1 5.8043 2.05357 3.92893 3.92893C2.05357 5.8043 1 8.34783 1 11V22.54C1 25.1922 2.05357 27.7357 3.92893 29.6111C5.8043 31.4864 8.34783 32.54 11 32.54H20.38C23.0322 32.54 25.5757 31.4864 27.4511 29.6111C29.3264 27.7357 30.38 25.1922 30.38 22.54V11C30.38 8.34783 29.3264 5.8043 27.4511 3.92893C25.5757 2.05357 23.0322 1 20.38 1Z"
                     fill="black"
                     stroke="white"
-                    stroke-width="2"
+                    strokeWidth="2"
                   />
                 </svg>
               </div>
             </div>
-            <div className="bg-white rounded-xl flex flex-row flex-wrap justify-between w-full h-fit p-[1.5rem]">
+            <div className="bg-white rounded-xl flex flex-row flex-wrap justify-between w-full h-fit p-[1rem]">
               <div className="flex flex-col">
-                <p className="text-[18px] text-[#616161] font-[400]">
+                <p className=" text-[#616161] font-[400]">
                   Subscription Period
                 </p>
-                <h1 className="text-lg font-semibold">15 Days</h1>
+                <h1 className="text-base font-semibold">15 Days</h1>
               </div>
               <div className="flex flex-col">
-                <p className="text-[18px] text-[#616161] font-[400]">
+                <p className=" text-[#616161] font-[400]">
                   Starting Date
                 </p>
-                <h1 className="text-lg font-semibold">01-03-2023</h1>
+                <h1 className="text-base font-semibold">01-03-2023</h1>
               </div>
               <div className="flex flex-col">
-                <p className="text-[18px] text-[#616161] font-[400]">
+                <p className=" text-[#616161] font-[400]">
                   Next Billing Date
                 </p>
-                <h1 className="text-lg font-semibold">15-03-2023</h1>
+                <h1 className="text-base font-semibold">15-03-2023</h1>
               </div>
               <div className="flex flex-col">
-                <p className="text-[18px] text-[#616161] font-[400]">
+                <p className=" text-[#616161] font-[400]">
                   Payment status
                 </p>
-                <h1 className="text-lg font-semibold">Pending</h1>
+                <h1 className="text-base font-semibold">Pending</h1>
               </div>
               <div className="flex flex-col">
-                <p className="text-[18px] text-[#616161] font-[400]">Status</p>
-                <h1 className="text-lg font-semibold">
-                  <span>Trail Period</span> (15 Days)
+                <p className=" text-[#616161] font-[400]">
+                  Status
+                </p>
+                <h1 className="text-base font-semibold">
+                  <span>Trial Period</span> (15 Days)
                 </h1>
               </div>
             </div>
           </div>
-          <div className="px-[1rem] md:px-[5rem] w-full  flex flex-row flex-wrap">
-            <div className="w-full flex flex-row justify-center items-center">
-              <div className="w-fit h-fit flex flex-row rounded-[40px] border border-1 border-[#000000CC]">
+          <div className="px-[1rem] md:px-[5rem] w-full flex flex-row flex-wrap">
+            <div className="w-full flex flex-row justify-center items-center -mt-2">
+              <div className="w-fit h-fit flex flex-row rounded-[2.5rem] border border-1 border-[#000000CC]">
                 <button
-                  className={`w-fit px-[1.5rem] py-2 font-[500] rounded-[40px] transition-colors duration-500 ${
+                  className={`w-fit px-[1.5rem] py-2 font-[500] rounded-[2.5rem] transition-colors duration-500 ${
                     switchTab === "quarterly"
                       ? "bg-[#004AAD] text-white"
                       : "bg-white text-black"
-                  } text-[20px]`}
+                  } text-[1.25rem]`}
                   onClick={() => setSwitchTab("quarterly")}
                 >
                   Quarterly
                 </button>
                 <button
-                  className={`w-fit px-[1.5rem] font-[500] rounded-[40px] transition-colors duration-500 ${
+                  className={`w-fit px-[1.5rem] font-[500] rounded-[2.5rem] transition-colors duration-500 ${
                     switchTab === "annual"
                       ? "bg-[#004AAD] text-white"
                       : "bg-white text-black"
-                  } text-[20px]`}
+                  } text-[1.25rem]`}
                   onClick={() => setSwitchTab("annual")}
                 >
                   Annually (save 44%)
--- src/pages/Stores.tsx
+++ src/pages/Stores.tsx
@@ -35,12 +35,12 @@ const Stores = () => {
   };
 
   return (
-    <div className="w-full h-fit relative ">
-      <div className=" w-[93%] h-fit px-[2rem] py-[1rem] flex flex-col items-center gap-2 justify-center ml-[7%]  ">
+    <div className="w-full h-fit relative">
+      <div className="w-[93%] h-fit px-[2rem] py-[1rem] flex flex-col items-center gap-2 justify-center ml-[7%]">
         <div className="w-full flex flex-row justify-between">
           <div className="flex flex-col -mt-3">
-            <h1 className="text-[24px] font-[500]">My stores</h1>
-            <p className="text-[16x]">
+            <h1 className="text-2xl font-[500]">My stores</h1>
+            <p className="text-[1rem]">
               You have{" "}
               <span className="text-[#004AAD] font-bold">
                 {stores.length} active store
@@ -48,40 +48,40 @@ const Stores = () => {
             </p>
           </div>
           <div className="w-fit h-fit">
-            <button className="w-[150px] flex flex-row justify-center items-center gap-2 bg-[#004AAD] text-white px-2 py-2 text-lg font-[500] rounded-[8px]">
+            <button className="w-[9.375rem] flex flex-row justify-center items-center gap-2 bg-[#004AAD] text-white px-2 py-2 text-lg font-[500] rounded-[0.5rem]">
               <FaPlus className="text-lg" />
               Add Store
             </button>
             <svg
-              className="relative -top-14 -right-[8.4rem] w-7 h-6"
-              width="32"
-              height="34"
+              className="relative -top-[3.5rem] -right-[8.4rem] w-[1.75rem] h-[1.5rem]"
+              width="2rem"
+              height="2.125rem"
               viewBox="0 0 32 34"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
               <rect
-                x="4.5"
-                y="4.5"
-                width="22.38"
-                height="24.54"
-                rx="11.19"
+                x="0.281rem"
+                y="0.281rem"
+                width="1.4rem"
+                height="1.534rem"
+                rx="0.699rem"
                 fill="white"
               />
               <rect
-                x="4.5"
-                y="4.5"
-                width="22.38"
-                height="24.54"
-                rx="11.19"
+                x="0.281rem"
+                y="0.281rem"
+                width="1.4rem"
+                height="1.534rem"
+                rx="0.699rem"
                 stroke="white"
-                stroke-width="5"
+                strokeWidth="0.313rem"
               />
               <path
                 d="M18.6236 14.7973L24.3745 13.2897L21.785 22.8177L21.7842 22.8206L21.7671 22.82H21.75H9.16H9.15184L9.14369 22.8201L7.41392 13.2914L7.41399 13.2914L7.41201 13.2811L7.42563 13.2861L7.44075 13.291L12.7608 15.021L13.6558 15.312L14.0005 14.4362L15.7095 10.0944L17.4486 14.2186L17.7853 15.0171L18.6236 14.7973ZM20.38 1H11C8.34783 1 5.8043 2.05357 3.92893 3.92893C2.05357 5.8043 1 8.34783 1 11V22.54C1 25.1922 2.05357 27.7357 3.92893 29.6111C5.8043 31.4864 8.34783 32.54 11 32.54H20.38C23.0322 32.54 25.5757 31.4864 27.4511 29.6111C29.3264 27.7357 30.38 25.1922 30.38 22.54V11C30.38 8.34783 29.3264 5.8043 27.4511 3.92893C25.5757 2.05357 23.0322 1 20.38 1Z"
                 fill="black"
                 stroke="white"
-                stroke-width="2"
+                strokeWidth="0.125rem"
               />
             </svg>
           </div>
@@ -91,62 +91,77 @@ const Stores = () => {
           {stores.map((store: StoreType) => (
             <div
               key={store.username}
-              className="w-full flex flex-col gap-4 rounded-xl px-8 py-4 h-fit bg-[#F1F7FF]"
+              className="w-full flex flex-col gap-4 rounded-xl px-[2rem] py-4 h-fit bg-[#F1F7FF]"
             >
               <div className="w-full flex flex-row justify-between">
                 <div className="flex flex-row gap-4">
-                  <img src={image} className="w-[60px] h-[60px] object-cover" />
+                  <img
+                    src={image}
+                    className="w-[3.75rem] h-[3.75rem] object-cover"
+                  />
                   <div className="flex flex-col">
-                    <h1 className="text-[24px] font-semibold">
+                    <h1 className="text-[1.5rem] font-semibold">
                       {store.username}
                     </h1>
-                    <p className="text-[18x] font-[400] text-[#616161]">
+                    <p className="text-[1.125rem] font-[400] text-[#616161]">
                       {store.email}
                     </p>
                   </div>
                 </div>
                 <div className="w-fit flex flex-col justify-center">
                   <button
-                    className="w-fit h-fit flex flex-row justify-center items-center gap-2 text-black text-[20px] font-[500] rounded-[8px]"
+                    className="w-fit h-fit flex flex-row justify-center items-center gap-2 text-black text-[1.25rem] font-[500] rounded-[0.5rem]"
                     onClick={() => toggleModal()}
                   >
-                    <BiEditAlt className="" />
+                    <BiEditAlt />
                     Edit Details
                   </button>
                 </div>
               </div>
-              <div className="bg-white rounded-xl flex flex-row flex-wrap gap-x-[1.2rem] gap-y-[2.2rem] w-fit h-fit p-[1.5rem]">
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
-                    Bussiness Type
+              <div className="bg-white rounded-xl flex flex-row flex-wrap gap-x-[0.75rem] gap-y-[1.375rem] w-fit h-fit p-[1.5rem]">
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-[1.125rem] text-[#616161] font-[400]">
+                    Business Type
                   </p>
-                  <h1 className=" text-xl font-semibold">{store.type}</h1>
+                  <h1 className="text-[1.125rem] font-semibold">
+                    {store.type}
+                  </h1>
                 </div>
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
-                    Bussiness Landmark
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-[1.125rem] text-[#616161] font-[400]">
+                    Business Landmark
                   </p>
-                  <h1 className="text-xl font-semibold">{store.landmark}</h1>
+                  <h1 className="text-[1.125rem] font-semibold">
+                    {store.landmark}
+                  </h1>
                 </div>
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-[1.125rem] text-[#616161] font-[400]">
                     Business City
                   </p>
-                  <h1 className="text-xl font-semibold">{store.city}</h1>
+                  <h1 className="text-[1.125rem] font-semibold">
+                    {store.city}
+                  </h1>
                 </div>
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">State</p>
-                  <h1 className="text-xl font-semibold">{store.state}</h1>
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-[1.125rem] text-[#616161] font-[400]">
+                    State
+                  </p>
+                  <h1 className="text-[1.125rem] font-semibold">
+                    {store.state}
+                  </h1>
                 </div>
-                <div className="w-[20px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-[1.125rem] text-[#616161] font-[400]">
                     Pincode
                   </p>
-                  <h1 className="text-xl font-semibold">{store.pincode}</h1>
+                  <h1 className="text-[1.125rem] font-semibold">
+                    {store.pincode}
+                  </h1>
                 </div>
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
-                    Social Handels
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-[1.125rem] text-[#616161] font-[400]">
+                    Social Handles
                   </p>
                   <div className="w-full flex flex-row items-center gap-4 mt-2">
                     {store.socials.map((social) => (
@@ -170,8 +185,8 @@ const Stores = () => {
                     ))}
                   </div>
                 </div>
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-lg text-[#616161] font-[400]">
                     Feedback Channels
                   </p>
                   <div className="w-full flex flex-row items-center gap-4 mt-2">
@@ -191,17 +206,17 @@ const Stores = () => {
                     ))}
                   </div>
                 </div>
-                <div className="w-[200px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
+                <div className=" w-[13rem] flex flex-col">
+                  <p className="text-lg text-[#616161] font-[400]">
                     Manager Name
                   </p>
-                  <h1 className="text-xl font-semibold">{store.manager}</h1>
+                  <h1 className="text-lg font-semibold">{store.manager}</h1>
                 </div>
-                <div className="w-[220px] flex flex-col">
-                  <p className="text-[18px] text-[#616161] font-[400]">
+                <div className="w-[13rem] flex flex-col">
+                  <p className="text-lg text-[#616161] font-[400]">
                     Manager Contact
                   </p>
-                  <h1 className="text-xl font-semibold">{store.contact}</h1>
+                  <h1 className="text-lg font-semibold">{store.contact}</h1>
                 </div>
               </div>
             </div>
@@ -217,15 +232,15 @@ const Stores = () => {
           className={`fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-end z-50 p-2`}
         >
           <div
-            className={`bg-white w-[500px] shadow-lg rounded-lg overflow-y-auto ${
+            className={`bg-white w-[31.25rem] shadow-lg rounded-lg overflow-y-auto h-full${
               isClosing ? "slide-out-right" : "slide-in-right"
             }`}
           >
-            <div className="w-full relative p-6 bg-white rounded-lg shadow">
+            <div className="w-full relative p-6 bg-white rounded-lg shadow h-full">
               <div className="flex flex-row justify-between">
                 <div className="flex flex-col">
-                  <h1 className="text-[24px] font-bold">Edit Store Details</h1>
-                  <p className="text-[16px] font-medium">
+                  <h1 className="text-2xl font-bold">Edit Store Details</h1>
+                  <p className="text-base font-medium">
                     Edit your store details at convinience
                   </p>
                 </div>
@@ -236,76 +251,81 @@ const Stores = () => {
                   className="text-4xl hover:cursor-pointer"
                 />
               </div>
-              <div className="flex flex-col mt-3">
-                <div className="flex justify-center pb-1">
-                  <img src={image} className="w-[70px] h-[70px] object-cover" />
+              <div className="flex flex-col  h-[90%] justify-around">
+                <div className="flex justify-center pb-1 ">
+                  <img
+                    src={image}
+                    className="w-[4.38rem] h-[4.38rem] object-cover"
+                  />
                 </div>
-                <form className="flex flex-col gap-4 justify-center">
-                  <div className="flex flex-col gap-2">
-                    <label className="flex flex-row items-center text-[14px] font-[500]">
-                      Outlet Name
-                      <LuAsterisk className="text-sm text-[#C62828]" />
-                    </label>
-                    <input
-                      type="text"
-                      className="w-full p-2 border-2 border-[#00000033] rounded-[8px] text-[18px]"
-                      placeholder="Enter Name"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <label
-                      htmlFor="country"
-                      className="flex flex-row items-center text-[14px] font-[500]"
-                    >
-                      Outlet Type
-                      <LuAsterisk className="text-sm text-[#C62828]" />
-                    </label>
-                    <select
-                      className="w-full p-2 border-2 border-[#00000033] rounded-[8px]"
-                      id="type"
-                      name="type"
-                      // value="dw"
-                      // onChange={handleInputChange}
-                    >
-                      <option value="">Select Type</option>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                    </select>
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <label className="flex flex-row items-center text-[14px] font-[500]">
-                      Outlet Mail ID
-                      <LuAsterisk className="text-sm text-[#C62828]" />
-                    </label>
-                    <input
-                      type="text"
-                      className="w-full p-2 border-2 border-[#00000033] rounded-[8px] text-[18px]"
-                      placeholder="Enter Email ID"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <label className="flex flex-row items-center text-[14px] font-[500]">
-                      Outlet Landmark
-                      <LuAsterisk className="text-sm text-[#C62828]" />
-                    </label>
-                    <input
-                      type="text"
-                      className="w-full p-2 border-2 border-[#00000033] rounded-[8px] text-[18px]"
-                      placeholder="Enter Landmark"
-                    />
-                  </div>
-                  <div className="flex flex-row gap-5 mt-1">
-                    <button
-                      className="w-[50%]  text-[1.1rem] rounded-[8px] border-2 font-bold text-richblack-900 px-[12px] py-2"
-                      onClick={() => handleCloseModal()}
-                    >
-                      Cancel
-                    </button>
-                    <button className="w-[50%] bg-[#004AAD]  text-[1.1rem] rounded-[8px] text-white font-bold text-richblack-900 px-[12px] py-2">
-                      Save
-                    </button>
+                <form className="flex flex-col gap-4 ">
+                  <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
+                      <label className="flex flex-row items-center text-sm font-[500]">
+                        Outlet Name
+                        <LuAsterisk className="text-sm text-[#C62828]" />
+                      </label>
+                      <input
+                        type="text"
+                        className="w-full p-2 border-2 border-[#00000033] rounded-[0.5rem] text-lg"
+                        placeholder="Enter Name"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <label
+                        htmlFor="country"
+                        className="flex flex-row items-center text-sm font-[500]"
+                      >
+                        Outlet Type
+                        <LuAsterisk className="text-sm text-[#C62828]" />
+                      </label>
+                      <select
+                        className="w-full p-2 border-2 border-[#00000033] rounded-[0.5rem]"
+                        id="type"
+                        name="type"
+                        // value="dw"
+                        // onChange={handleInputChange}
+                      >
+                        <option value="">Select Type</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                      </select>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <label className="flex flex-row items-center text-sm font-[500]">
+                        Outlet Mail ID
+                        <LuAsterisk className="text-sm text-[#C62828]" />
+                      </label>
+                      <input
+                        type="text"
+                        className="w-full p-2 border-2 border-[#00000033] rounded-[0.5rem] text-lg"
+                        placeholder="Enter Email ID"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <label className="flex flex-row items-center text-sm font-[500]">
+                        Outlet Landmark
+                        <LuAsterisk className="text-sm text-[#C62828]" />
+                      </label>
+                      <input
+                        type="text"
+                        className="w-full p-2 border-2 border-[#00000033] rounded-[0.5rem] text-3"
+                        placeholder="Enter Landmark"
+                      />
+                    </div>
                   </div>
                 </form>
+                <div className="flex flex-row gap-5 mt-3">
+                  <button
+                    className="w-[50%]  text-[1.1rem] rounded-[8px] border-2 font-bold text-richblack-900 px-3 py-2"
+                    onClick={() => handleCloseModal()}
+                  >
+                    Cancel
+                  </button>
+                  <button className="w-[50%] bg-[#004AAD]  text-[1.1rem] rounded-[8px] text-white font-bold text-richblack-900 px-3 py-2">
+                    Save
+                  </button>
+                </div>
               </div>
             </div>
           </div>
