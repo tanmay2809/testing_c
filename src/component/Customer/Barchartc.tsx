ebc590f4b8166d869b3e3373d963eb245da965c2
Vani Gupta <118043711+Vani-1107@users.noreply.github.com>
2024-07-26 22:20:51 +0530

    Update Barchartc.tsx

--- src/component/Customer/Barchartc.tsx
+++ src/component/Customer/Barchartc.tsx
@@ -1,62 +1,101 @@
 
-import { Chart ,GoogleChartOptions} from "react-google-charts";
+// import { Chart ,GoogleChartOptions} from "react-google-charts";
 
 
-export const data = [
-  ["month", "Sales"],
-  ["1 jan", 1000],
-  ["2 jan", 1170],
-  ["3 jan", 660],
-  ["4 jan", 1030],
-  ["5 jan", 1000],
-  ["6 jan", 1170],
-  ["7 jan", 660],
-  ["8 jan", 1030],
-  ["9 jan", 1000],
-  ["10 jan", 1170],
-  ["11 jan", 660],
-  ["12 jan", 1030],
-  ["13 jan", 1000],
-  ["14 jan", 1170],
-  ["15 jan", 660],
-  ["16 jan", 1030],
-  ["17 jan", 1000],
-  ["18 jan", 1170],
-  ["19 jan", 660],
-  ["20 jan", 1030],
-  ["21 jan", 1000],
-  ["22 jan", 1170],
-  ["23 jan", 660],
-  ["24 jan", 1030],
-  ["25 jan", 660],
-  ["26 jan", 1030],
-  ["27 jan", 1000],
-  ["28 jan", 1170],
-  ["29 jan", 660],
-  ["30 jan", 1030],
-];
+// export const data = [
+//   ["month", "Sales"],
+//   ["1 jan", 1000],
+//   ["2 jan", 1170],
+//   ["3 jan", 660],
+//   ["4 jan", 1030],
+//   ["5 jan", 1000],
+//   ["6 jan", 1170],
+//   ["7 jan", 660],
+//   ["8 jan", 1030],
+//   ["9 jan", 1000],
+//   ["10 jan", 1170],
+//   ["11 jan", 660],
+//   ["12 jan", 1030],
+//   ["13 jan", 1000],
+//   ["14 jan", 1170],
+//   ["15 jan", 660],
+//   ["16 jan", 1030],
+//   ["17 jan", 1000],
+//   ["18 jan", 1170],
+//   ["19 jan", 660],
+//   ["20 jan", 1030],
+//   ["21 jan", 1000],
+//   ["22 jan", 1170],
+//   ["23 jan", 660],
+//   ["24 jan", 1030],
+//   ["25 jan", 660],
+//   ["26 jan", 1030],
+//   ["27 jan", 1000],
+//   ["28 jan", 1170],
+//   ["29 jan", 660],
+//   ["30 jan", 1030],
+// ];
 
-export const options: GoogleChartOptions = {
-  title: "",
-  is3D: false,
-  colors: ['#004AAD'],
-  backgroundColor: '#F1F7FF',
-  chartArea: {
-    backgroundColor: '#F1F7FF'
-  }
-};
+// export const options: GoogleChartOptions = {
+//   title: "",
+//   is3D: false,
+//   colors: ['#004AAD'],
+//   backgroundColor: '#F1F7FF',
+//   chartArea: {
+//     backgroundColor: '#F1F7FF'
+//   }
+// };
 
-export function BarChartc() {
-  return (
+// export function BarChartc() {
+//   return (
     
-      <Chart
-        className=""
-        chartType="Bar"
-        width="108%"
-        height="300px"
-        data={data}
-        options={options}
-      />
+//       <Chart
+//         className=""
+//         chartType="Bar"
+//         width="108%"
+//         height="300px"
+//         data={data}
+//         options={options}
+//       />
     
-  );
+//   );
+// }
+
+import { Chart, GoogleChartOptions } from "react-google-charts";
+
+interface BarChartcProps {
+  dailyVisits: { [key: string]: number };
 }
+
+export const BarChartc: React.FC<BarChartcProps> = ({ dailyVisits }) => {
+  const data = [["Date", "Visits"]];
+  const sortedDates = Object.keys(dailyVisits).sort((a, b) => {
+    const dateA = parseInt(a.split(" ")[0]);
+    const dateB = parseInt(b.split(" ")[0]);
+    return dateA - dateB;
+  });
+
+  sortedDates.forEach((date) => {
+    data.push([date, dailyVisits[date]]);
+  });
+
+  const options: GoogleChartOptions = {
+    title: "",
+    is3D: false,
+    colors: ["#004AAD"],
+    backgroundColor: "#F1F7FF",
+    chartArea: {
+      backgroundColor: "#F1F7FF",
+    },
+  };
+
+  return (
+    <Chart
+      chartType="Bar"
+      width="108%"
+      height="300px"
+      data={data}
+      options={options}
+    />
+  );
+};
