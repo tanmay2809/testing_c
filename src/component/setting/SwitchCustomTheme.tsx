49df569d446a3cb3df54c543b77f911d1aa24330
Nitish kumar <139990315+Nitish0604@users.noreply.github.com>
2024-08-17 02:53:20 +0530

    update

---
+++ src/component/setting/SwitchCustomTheme.tsx
@@ -0,0 +1,32 @@
+import { useState,useEffect } from "react";
+
+
+
+const SwitchCustomTheme = () => {
+
+    const [switc, setSwitch] = useState<boolean | undefined>(true);
+
+  useEffect(() => {
+    setSwitch(true);
+  }, [true]);
+
+  const handleClick = () => {
+    setSwitch(!switc);
+    
+  };
+  return (
+    <div onClick={handleClick} className="cursor-pointer">
+      {!switc ? (
+        <div className="w-[40px] h-[22px] bg-slate-400 rounded-full flex items-center">
+          <div className="size-[18px] bg-white rounded-full ml-[0.15rem]"></div>
+        </div>
+      ) : (
+        <div className="w-[40px] h-[22px] bg-green-400 rounded-full flex items-center">
+          <div className="size-[18px] bg-white rounded-full ml-5"></div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default SwitchCustomTheme
--- src/pages/Settings/CustomiseTheme.tsx
+++ src/pages/Settings/CustomiseTheme.tsx
@@ -1,10 +1,39 @@
-// React import removed as it's not used in this component
+//icons
+import { FiPlus } from "react-icons/fi";
+import { MdDeleteForever } from "react-icons/md";
+import { RxCrossCircled } from "react-icons/rx";
+
 
 import { useState } from "react";
 
 import mobile from "../../assets/mobile.png";
+import mobile2 from "../../assets/Group 1171278587.png";
+
+import zomato from "../../assets/Rectangle 3463841.png";
+import swiggy1 from "../../assets/Rectangle 3463840 (1).png"
+
+
+import swiggy from "../../assets/Rectangle 3463840.png";
+import SwitchCustomTheme from "../../component/setting/SwitchCustomTheme";
+
+
 
 const CustomiseTheme = () => {
+   
+
+    const delivery = [{
+        icon: zomato,
+        Link: "www.zomato.com/foodoos/Book Now.."
+    }, {
+        icon: swiggy1,
+        Link: "www.Swiggydineout.com/foodoos/Book..."
+    },
+    {
+        icon: swiggy1,
+        Link: "www.Swiggydineout.com/foodoos/Book..."
+    }]
+
+    const [adddelivery, setadddelivery] = useState<boolean>(false);
 
     // navbar frame
     const handleFrame = () => {
@@ -31,15 +60,15 @@ const CustomiseTheme = () => {
                     </div>
                 </div>
 
-                <div className="w-full h-fit flex gap-2   rounded-lg py-[1rem]  ">
+                <div className="w-full h-fit flex gap-4   rounded-lg py-[1rem]  ">
                     {/* right */}
-                    <div className="w-[50%] h-fit  bg-white flex  flex-col justify-center items-center gap-[3rem] pt-[2rem] rounded-lg ">
+                    <div className="w-[50%] h-fit pb-[3rem] bg-white flex  flex-col justify-center items-center gap-[3rem] pt-[2rem] rounded-lg ">
 
                         <div className="w-fit h-fit flex flex-row rounded-[2.5rem] border border-1 border-[#000000CC] ">
                             <button
                                 className={`w-fit px-[1.5rem] py-2 font-[500] rounded-[2.5rem] transition-colors duration-500 ${switchTab === "Dine-in Preview"
-                                        ? "bg-[#004AAD] text-white"
-                                        : "bg-white text-black"
+                                    ? "bg-[#004AAD] text-white"
+                                    : "bg-white text-black"
                                     } text-[1.25rem]`}
                                 onClick={() => setSwitchTab("Dine-in Preview")}
                             >
@@ -47,8 +76,8 @@ const CustomiseTheme = () => {
                             </button>
                             <button
                                 className={`w-fit px-[1.5rem] font-[500] rounded-[2.5rem] transition-colors duration-500 ${switchTab === "Public Profile Preview"
-                                        ? "bg-[#004AAD] text-white"
-                                        : "bg-white text-black"
+                                    ? "bg-[#004AAD] text-white"
+                                    : "bg-white text-black"
                                     } text-[1.25rem]`}
                                 onClick={() => setSwitchTab("Public Profile Preview")}
                             >
@@ -59,8 +88,8 @@ const CustomiseTheme = () => {
 
                         {
                             switchTab === "Dine-in Preview" ? (
-                            <img src={mobile} alt="mobile" className="w-[230px]  aspect-auto object-cover" />
-                            ) : (<img src={mobile} alt="mobile" className="w-[235px] aspect-auto object-cover" />)
+                                <img src={mobile} alt="mobile" className="w-[230px]  aspect-auto object-cover" />
+                            ) : (<img src={mobile2} alt="mobile" className="w-[235px] aspect-auto object-cover" />)
                         }
 
                     </div>
@@ -69,22 +98,75 @@ const CustomiseTheme = () => {
 
                     <div className="w-[50%] h-fit flex flex-col justify-center items-center gap-[1rem]  ">
 
-                        <p className="w-full font-bold text-[1.3rem] text-black bg-white px-[2rem] py-2 rounded-lg items-center">Colour Theme Control</p>
+                        {/* colour theme */}
 
-                        <div className="w-full font-semibold text-[1.2rem] text-black bg-white px-[2rem] py-2 rounded-lg items-center flex flex-col gap-[.8rem]">
-                            <div className=" flex item items-center justify-between w-full">
-                                <p>Primary Brand Colour</p>
-                                <div className="flex items-center gap-[1rem]">
-                                 <input type="color" className="w-[100px] h-[40px]  border rounded-xl" />
-                                </div>  
-                            </div>
-                            <div className=" flex item items-center justify-between w-full">
-                                <p>Secondary Colour</p>
-                                <div className="flex items-center gap-[1rem]">
-                                 <input type="color" className="w-[100px] h-[40px]  border rounded-xl" />
-                                </div>  
+                        <div className="flex flex-col gap-[.5rem] w-full h-fit">
+                            <p className="w-full font-bold text-[1.3rem] text-black bg-white px-[2rem] py-3 rounded-t-lg items-center">Colour Theme Control</p>
+
+                            <div className="w-full font-semibold text-[1.2rem] text-black bg-white px-[2rem] py-3 rounded-b-lg items-center flex flex-col gap-[.8rem]">
+                                <div className=" flex item items-center justify-between w-full">
+                                    <p>Primary Brand Colour</p>
+                                    <div className="flex items-center gap-[1rem]">
+                                        <input type="color" className="w-[100px] h-[40px]  border rounded-xl" />
+                                    </div>
+                                </div>
+                                <div className=" flex item items-center justify-between w-full">
+                                    <p>Secondary Colour</p>
+                                    <div className="flex items-center gap-[1rem]">
+                                        <input type="color" className="w-[100px] h-[40px]  border rounded-xl" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
+                        {/* booking links */}
+
+                        <div className="flex flex-col gap-[.5rem] w-full h-fit">
+                            <div className="w-full font-bold text-[1.3rem] text-black bg-white px-[2rem] py-3 rounded-t-lg items-center flex justify-between">
+                                <p>Booking Links</p>
+                                <button className="text-[#004AAD] font-semibold text-[1.3rem] flex items-center gap-3"> <FiPlus className="font-bold size-8" /> Add New</button>
+                            </div>
+
+                            <div className="w-full  bg-white px-[2rem] py-3 rounded-b-lg items-center flex  gap-[1rem]">
+                                <img src={swiggy} alt="swiggy" className="w-[60px] aspect-auto object-cover" />
+                                <p className="w-[70%] px-4 rounded-lg py-2 border border-black font-[400] text-black text-[1rem]">www.zomato.com/foodoos/Book Now</p>
+                                <div className="flex gap-4 items-center ">
+                                    <SwitchCustomTheme />
+                                    <MdDeleteForever className="text-[#004AAD] size-8" />
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+                        {/* Delivery Links */}
+
+                        <div className="flex flex-col gap-[.5rem] w-full h-fit">
+                            <div className="w-full font-bold text-[1.3rem] text-black bg-white px-[2rem] py-3 rounded-t-lg items-center flex justify-between">
+                                <p>Delivery Links</p>
+                                <button className="text-[#004AAD] font-semibold text-[1.3rem] flex items-center gap-3"> <FiPlus className="font-bold size-8" /> Add New</button>
+                            </div>
+                            {
+                                delivery.map((item, index) => (
+                                    <div key={index} className="w-full  bg-white px-[2rem] py-3 rounded-b-lg items-center flex  gap-[1rem]">
+                                        <img src={item.icon} alt="swiggy" className="w-[60px] aspect-auto object-cover" />
+                                        <p className="w-[70%] h-[40px] px-4 rounded-lg py-2 border border-black font-[400] text-black text-[1rem]">{item.Link}</p>
+                                        <div className="flex gap-4 items-center ">
+                                            <SwitchCustomTheme />
+                                            <MdDeleteForever className="text-[#004AAD] size-8" />
+                                        </div>
+
+                                    </div>
+                                ))
+                            }
+
+
+
+                        </div>
+
+
+
 
                     </div>
 
@@ -99,6 +181,26 @@ const CustomiseTheme = () => {
                 </div>
 
             </div>
+
+            {
+                adddelivery && (
+                    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
+                        <div className="bg-white w-[40%] h-[60%] rounded-lg py-6">
+                            <div className="flex justify-between items-center text-[#0F172A] font-Roboto font-[500] text-[1.8rem] px-6 border-b border-b-[#0F172A] pb-3">
+                                <p>Add Delivery Link</p>
+                                <RxCrossCircled onClick={() => setadddelivery(false)} className="cursor-pointer" />
+                            </div>
+
+                            
+                        </div>
+
+                    </div>
+                )
+
+            }
+
+
+
 
 
         </div>
